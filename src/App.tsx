632a169970e36d81cc3dad197c14c07e6406e080
Christo-lk <73858996+Christo-lk@users.noreply.github.com>
2022-03-26 16:05:47 +1300

    set up redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import NewNoteInput  from "./NewNoteInput"
+import { NotesState } from './notesReducer';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    //6:29
+    // use selector return type at 7:16
+    const notes = useSelector<NotesState, NotesState["notes"]>((state) => state.notes)
+    const dispatch = useDispatch()
+
+    console.log("notes: ", notes)
+
+    const addNote = (note: string) => {
+        dispatch({
+            type: "ADD_NOTE",
+            payload: note
+        })
+    }
+
+    return (
+        <div className="App">
+            <h1>test on air</h1>
+            <NewNoteInput addNote={addNote}/>
+            
+            <hr></hr>
+
+            {notes.map((note, index) => {
+                 return <div className="square" />
+                })}
+            <ul>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
